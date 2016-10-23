26e7d788d4153f8d39504180e482eb4e7d90b8e9
ddworak <dawid.dworak@gmail.com>
2016-10-23 15:36:55 +0000

    enrich interface definition, add config library

--- build.sbt
+++ build.sbt
@@ -7,11 +7,14 @@ scalaVersion := "2.11.8"
 cancelable in Global := true
 
 val Version = new {
+  val typesafeConfig = "1.3.1"
   val scalaLogging = "3.5.0"
   val logback = "1.1.7"
   val scalatest = "3.0.0"
 }
+
 libraryDependencies ++= Seq(
+  "com.typesafe" % "config" % Version.typesafeConfig,
   "com.typesafe.scala-logging" %% "scala-logging" % Version.scalaLogging,
   "ch.qos.logback" % "logback-classic" % Version.logback,
   "org.scalatest" %% "scalatest" % Version.scalatest % "test"
--- src/main/scala/me/dworak/mesosphere/rocket/ElevatorControlSystem.scala
+++ src/main/scala/me/dworak/mesosphere/rocket/ElevatorControlSystem.scala
@@ -1,13 +1,13 @@
 package me.dworak.mesosphere.rocket
 
 trait ElevatorControlSystem {
-  def status: Map[ElevatorId, ElevatorState]
+  def status: Map[ElevatorId, ElevatorStatus]
 
-  def status(elevatorId: ElevatorId): Option[ElevatorState]
+  def status(elevatorId: ElevatorId): Option[ElevatorStatus]
 
-  def update(elevatorState: ElevatorState): Unit
+  def update(elevatorState: ElevatorStatus): Unit
 
-  def pickup(sourceFloor: FloorId, up: Boolean): Unit
+  def pickup(sourceFloor: FloorId, up: Boolean): ElevatorId
 
   def step(): Unit
 }
@@ -16,4 +16,26 @@ case class FloorId(value: Int) extends AnyVal
 
 case class ElevatorId(value: Int) extends AnyVal
 
-case class ElevatorState(elevatorId: ElevatorId, currentFloor: FloorId, destinationFloors: List[FloorId])
+case class Ticks(value: Int) extends AnyVal
+
+sealed trait Direction
+
+object Direction {
+
+  case object Up extends Direction
+
+  case object Down extends Direction
+
+  case object Open extends Direction
+
+}
+
+trait TimeAssumption {
+  def ticksPerFloor: Ticks
+
+  def ticksOpen: Ticks
+}
+
+case class Position(floor: FloorId, offset: Ticks, direction: Direction)
+
+case class ElevatorStatus(elevatorId: ElevatorId, position: Position, destinationFloors: List[FloorId])
