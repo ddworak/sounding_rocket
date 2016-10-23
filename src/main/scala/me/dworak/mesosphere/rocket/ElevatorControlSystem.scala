e4716b78e342fe3dfbaf794c42113007aff45d4a
ddworak <dawid.dworak@gmail.com>
2016-10-23 15:18:17 +0000

    fix interface typesafety

--- src/main/scala/me/dworak/mesosphere/rocket/ElevatorControlSystem.scala
+++ src/main/scala/me/dworak/mesosphere/rocket/ElevatorControlSystem.scala
@@ -1,11 +1,19 @@
 package me.dworak.mesosphere.rocket
 
 trait ElevatorControlSystem {
-  def status(): Seq[(Int, Int, Int)]
+  def status: Map[ElevatorId, ElevatorState]
 
-  def update(elevatorId: Int, sourceFloor: Int, destinationFloor: Int): Unit
+  def status(elevatorId: ElevatorId): Option[ElevatorState]
 
-  def pickup(sourceFloor: Int, destinationFloor: Int): Unit
+  def update(elevatorState: ElevatorState): Unit
+
+  def pickup(sourceFloor: FloorId, up: Boolean): Unit
 
   def step(): Unit
 }
+
+case class FloorId(value: Int) extends AnyVal
+
+case class ElevatorId(value: Int) extends AnyVal
+
+case class ElevatorState(elevatorId: ElevatorId, currentFloor: FloorId, destinationFloors: List[FloorId])
